542cb40747ecb328e8e9e7d549c9cf28c93c391a
Viking Studios <mrhowe95@gmail.com>
2024-08-27 23:23:40 +0100

    forgot this

--- VSMod/__init__.py
+++ VSMod/__init__.py
@@ -1,6 +1,12 @@
 from .vsmod import VSMod
 
-__red_end_user_data_statement__ = 'data such as UIDs of server members and Moderators along with GuildID and RoleID may be stored by the cog for keeping track of user warnings, mutes, bans, and offenses'
+__red_end_user_data_statement__ = (
+    'Data such as UIDs of server members and Moderators along with GuildID and RoleID may be stored by the cog for keeping track of user warnings, mutes, bans, and offenses.'
+)
 
 async def setup(bot):
-    await bot.add_cog(VSMod(bot))
+    cog = VSMod(bot)
+    await bot.add_cog(cog)
+
+    # Sync the slash commands globally or to a specific guild
+    await bot.tree.sync()
